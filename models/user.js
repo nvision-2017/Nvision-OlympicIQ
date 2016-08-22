d0837bddcef8ba6fda7923854a7161dcb73c4b84
thecodesome <cs15btech11018@iith.ac.in>
2016-08-22 21:12:30 +0530

    UI

--- models/user.js
+++ models/user.js
@@ -1,25 +1,46 @@
 const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
+
     id: String,
+   
     displayName: String,
+   
     email: String,
+    
     picture: String,
+    
     facebook: {
+   
         id: String,
+   
         token: String
+   
     },
+   
     games: [{
+   
         id: Number,
+   
         questions: [{
+    
             id: Number,
+    
             response: Number,
+    
             score: Boolean,
+    
             time: Number
+    
         }],
+    
         time: Number,
+    
         score : Number
+    
     }],
+   
     questions: [Number]
+
 });
 const User = mongoose.model('User', userSchema);
 module.exports = User;
