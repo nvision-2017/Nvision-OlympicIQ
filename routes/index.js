f3c4db867012a72761aa75c5ccb77b67dfe9cf77
Prateek Kumar <prateek@prateekkumar.in>
2016-08-22 11:11:40 +0530

    working game reset timer from each question to whole game

--- routes/index.js
+++ routes/index.js
@@ -31,44 +31,45 @@ router.get('/', ensureLoggedIn.ensureLoggedIn(), function (req, res) {
 
 router.get('/play', ensureLoggedIn.ensureLoggedIn(), function (req, res) {
     var user = req.user;
-    if (req.query.game == "new") {
-        if (user.games.length > 4) return res.render('game', {error: "No more games can be played"});
-        var user = req.user;
-        var game = {};
-        game.id = user.games.length;
-        game.questions = [];
-        game.time = Date.now();
-        game.score = 0;
-        user.games.push(game);
-        user.save(function (err, user) {
-            if (err) {
-                console.log(err);
-                return res.sendStatus(500);
-            }
-            else res.render('game', {id: user.games.length-1});
-        });
-    }
-    else {
-        var gid = Number(req.query.game);
-        res.render('game', {id: gid});
-    }
+    // if (user.games.length > 4) return res.render('game', {error: "No more games can be played"});
+    var game = {};
+    game.id = user.games.length;
+    game.questions = [];
+    game.time = Date.now();
+    game.score = 0;
+    user.games.push(game);
+    user.save(function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        else res.render('game', {game: game});
+    });
 });
 
 router.post('/play', ensureLoggedIn.ensureLoggedIn(), function (req, res) {
+    var user = req.user;
     var t = Date.now();
     var gameID = Number(req.body.gameID);
+    if ((!gameID && gameID != 0) && (gameID < 0 || gameID >= user.games.length )) return res.sendStatus(404);
     var answer = Number(req.body.answer);
-    // TODO Validate gameID and answer
-    var user = req.user;
+    var tl = (Math.abs(user.games[gameID].time-t) > 10000);
+    if (tl) {
+        return res.send({score: user.games[gameID].score});
+    }
+    if ((!answer && answer != 0) && (answer < 0 || answer > 3)) return res.sendStatus(404);
     var q = user.games[gameID].questions.length;
-    if (q != 0 && user.games[gameID].questions[q-1].response == undefined && Math.abs(user.games[gameID].questions[q-1].time-t) < 10000) {
+    if (q != 0 && user.games[gameID].questions[q-1].response == undefined && !tl) {
         user.games[gameID].questions[q-1].response = answer;
         user.games[gameID].questions[q-1].score = (Questions[user.games[gameID].questions[q-1].id].answer == answer);
         user.games[gameID].score += (user.games[gameID].questions[q-1].score ? 1:0);
     }
-    if (q != 10) {
+    if (q != 8 && !tl) {
         var qno = user.questions.pop();
         user.games[gameID].questions.push({id: qno, time: Date.now(), score: false});
+    }
+    if (q == 0) {
+        user.games[gameID].time = t;
     }
     user.save(function (err, user) {
         if (err) {
@@ -76,10 +77,10 @@ router.post('/play', ensureLoggedIn.ensureLoggedIn(), function (req, res) {
             res.sendStatus(500);
         }
         else {
-            if (q == 10) {
+            if (q == 8) {
                 return res.send({score: user.games[gameID].score});
             }
-            else res.send({question: Questions[qno].question, options: Questions[qno].options, time: t}); // TODO
+            else res.send({question: Questions[qno].question, options: Questions[qno].options, time: user.games[gameID].time}); // TODO
         }
     });
 });
